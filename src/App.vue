f46246b716a95bf81d0e782c91734460d7ce9aac
bayramkeles61 <79147272+bayramkeles61@users.noreply.github.com>
2022-06-13 15:32:30 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import Hello from "./components/Hello.vue";
+</script>
+
+<template>
+  <div>
+    <Hello msg="The Matrix has you..." />
+  </div>
+</template>
+
+<style>
+html {
+  overflow: hidden;
+  height: 100%;
+  width: 100%;
+  background: black;
+  color: rgb(3, 245, 3);
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import "@unocss/reset/tailwind.css"
+import "uno.css"
+import "virtual:fonts.css";
+
+createApp(App).mount('#app')
